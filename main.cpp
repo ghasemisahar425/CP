4649800495e9c961fe0c0741d6fc737229102b5d
ghasemisahar425 <115500048+ghasemisahar425@users.noreply.github.com>
2022-12-12 07:13:02 +0330

    practice p9-3
    practice p9-3

--- main.cpp
+++ main.cpp
@@ -1,32 +1,19 @@
 #include <iostream>
-#define x 10
-
- using namespace std;
+#define n 10
+using namespace std;
 
 int main(int argc, char** argv) {
-int array[x]={99,12,15,18,20,22,24,26,28,30};
-int i,j,w,min=0,temp;
-
-	for(i=0;i<x;i++){
-		cout<<"array value : "<<array[i]<<"\n";
-//		cin>>array[i];
-	}
-	for(i=0;i<x;i++){
-		for(j=0;j<x-1;j++){
-			if(array[i]<array[j]){
-				temp=array[i];
-				array[i]=array[j];
-				array[j]=temp;
-				cout<<"moratab shode:"<<array[j]<<"\n";
-			}
-		}
-	}
-	min=array[0];
-	for(i=1;i<x;i++){
-			if(array[i]<min){
-				min=array[i];
-			}
-}
-				cout<<"the min number is:"<<min<<"\n";
-				cout<<"the second min number is:"<<array[1];
+int array[n];
+int a=0,b=1,c,x,temp,i;
+	
+	cout<<"please enter your request number:";
+		cin>>array[n];
+			for(i=0;i<n;i++){
+				c=a+b;
+				a=b;
+				b=c;
+				array[i]=a;
+				cout<<"the fib position in array is: "<<i<<"  =  ";
+				cout<<"the fib number are :"<<a<<endl;
+			}	
 }
