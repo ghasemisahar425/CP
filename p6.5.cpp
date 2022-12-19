b2a10c2ffd6de5fd82fa23853140f5e95f47e6fa
ghasemisahar425 <115500048+ghasemisahar425@users.noreply.github.com>
2022-12-19 18:56:35 +0330

    practice p6-5
    practice p6-5

---
+++ p6.5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main(){
+
+int Num1,Num2,i,GCD;
+	cout<<"please enter tow integer values; ";
+	cin>>Num1>>Num2;
+		for(i=1;i<=Num1 && i<=Num2;i++){
+			if(Num1%i==0 && Num2%i==0){
+				GCD=i;
+			}
+		} 
+	cout<<"GCD of"<<Num1<<"and"<<Num2<<"is"<<GCD;
+}
+
